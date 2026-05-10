e8e48199d92f36ddfb1b0f958cb43743eeb05eb8
SalmaArifahZahra <136414163+SalmaArifahZahra@users.noreply.github.com>
2026-05-10 09:49:06 +0700

    merge into cloud-iuran

---
+++ lib/views/pages/warga/profile/kontak_pengurus_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KontakPengurusPage extends StatelessWidget {
+  const KontakPengurusPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Kontak Pengurus")),
+      body: const Center(child: Text("Halaman Kontak Pengurus")),
+    );
+  }
+}
