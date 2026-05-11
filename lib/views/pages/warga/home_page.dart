1dfbd34a1d579138fb27d7d53837217852cfd5a5
SalmaArifahZahra <136414163+SalmaArifahZahra@users.noreply.github.com>
2026-05-11 10:42:19 +0700

    merge cloud-surat #2

--- lib/views/pages/warga/home_page.dart
+++ lib/views/pages/warga/home_page.dart
@@ -4,7 +4,7 @@ import 'package:rukun_app_proyek4/utils/appbar_utils.dart';
 import 'package:rukun_app_proyek4/utils/colors_utils.dart';
 import 'package:rukun_app_proyek4/utils/menucard_utils.dart';
 import 'package:rukun_app_proyek4/viewmodels/auth_viewmodel.dart';
-import 'package:rukun_app_proyek4/views/pages/warga/pengajuan_surat_page.dart';
+import 'package:rukun_app_proyek4/views/pages/warga/surat/pengajuan_surat_page.dart';
 import 'package:rukun_app_proyek4/views/pages/warga/kegiatan_page.dart';
 import 'package:rukun_app_proyek4/views/pages/warga/iuran/iuran_page.dart';
 
