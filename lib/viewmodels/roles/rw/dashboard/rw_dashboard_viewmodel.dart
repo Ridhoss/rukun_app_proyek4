d7f823f9ac880f1cb4e2ac9128882012c300d250
SalmaArifahZahra <136414163+SalmaArifahZahra@users.noreply.github.com>
2026-05-24 13:15:18 +0700

    feat: dashboard rw

--- lib/viewmodels/roles/rw/dashboard/rw_dashboard_viewmodel.dart
+++ lib/viewmodels/roles/rw/dashboard/rw_dashboard_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rukun_app_proyek4/models/dashboard_model.dart';
+import 'package:rukun_app_proyek4/models/kegiatan_model.dart';
 import 'package:rukun_app_proyek4/repositories/dashboard_repository.dart';
 
 class DashboardRwViewModel extends ChangeNotifier {
@@ -12,6 +13,7 @@ class DashboardRwViewModel extends ChangeNotifier {
 
   DashboardModel? dashboard;
 
+  // dummy
   final double saldoKas = 8450000;
   final double kasMasuk = 12300000;
   final double kasKeluar = 3850000;
@@ -23,11 +25,11 @@ class DashboardRwViewModel extends ChangeNotifier {
       notifyListeners();
 
       final result = await repository.getDashboardRW();
-      print("FETCH BERHASIL");
-      print(result.toJson());
+
       dashboard = result;
     } catch (e) {
       errorMessage = e.toString();
+      debugPrint("ERROR DASHBOARD RW: $e");
     } finally {
       isLoading = false;
       notifyListeners();
@@ -36,14 +38,27 @@ class DashboardRwViewModel extends ChangeNotifier {
 
   int get totalPenduduk => dashboard?.totalPenduduk ?? 0;
   int get totalKK => dashboard?.jumlahKk ?? 0;
-  int get totalRT => dashboard?.rt.rwId ?? 0;
+  int get totalRT => dashboard?.jumlahRt ?? 0;
   int get totalSurat => dashboard?.statusSurat.total ?? 0;
   int get suratPending => dashboard?.statusSurat.diajukan ?? 0;
   int get suratDiproses => dashboard?.statusSurat.disetujui ?? 0;
+  int get suratSelesai => dashboard?.statusSurat.selesai ?? 0;
+  int get suratDitolak => dashboard?.statusSurat.ditolak ?? 0;
+  int get pria => dashboard?.gender.pria ?? 0;
+  int get wanita => dashboard?.gender.wanita ?? 0;
   int get anak => dashboard?.ageDistribution.anak ?? 0;
   int get produktif => dashboard?.ageDistribution.produktif ?? 0;
   int get lansia => dashboard?.ageDistribution.lansia ?? 0;
-  int get pria => dashboard?.gender.pria ?? 0;
-  int get wanita => dashboard?.gender.wanita ?? 0;
-  List get kegiatan => dashboard?.kegiatan ?? [];
+
+
+  List<PendudukPerRT> get pendudukPerRt => dashboard?.pendudukPerRt ?? [];
+  List<Kegiatan> get kegiatan => dashboard?.kegiatan ?? [];
+
+  Kegiatan? get kegiatanAktif {
+    try {
+      return kegiatan.firstWhere((e) => e.isBerlangsung);
+    } catch (_) {
+      return null;
+    }
+  }
 }
